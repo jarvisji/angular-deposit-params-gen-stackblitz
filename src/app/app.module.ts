6f8f723662b92d1f7763ec55a74195b06bbcd28d
jarvisji <jarvis.ji@outlook.com>
2020-03-15 16:55:02 +0800

    Init form for new product.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { DepositeProductDetailsComponent } from "./deposite-product-details/deposite-product-details.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   declarations: [AppComponent, HelloComponent, DepositeProductDetailsComponent],
   bootstrap: [AppComponent]
 })
--- src/app/deposite-product-details/deposite-product-details.component.ts
+++ src/app/deposite-product-details/deposite-product-details.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-deposite-product-details',
-  templateUrl: './deposite-product-details.component.html',
-  styleUrls: ['./deposite-product-details.component.css']
+  selector: "app-deposite-product-details",
+  templateUrl: "./deposite-product-details.component.html",
+  styleUrls: ["./deposite-product-details.component.css"]
 })
 export class DepositeProductDetailsComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  dppForm = this.fb.group({
+    productName: ["", Validators.required]
+  });
+
+  onSubmite() {
+    console.warn(this.dppForm.value);
   }
-
 }
