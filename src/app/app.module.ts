b2741ca801056e3923c5ba76a77b985360f1a343
jarvisji <jarvis.ji@outlook.com>
2020-03-15 23:30:07 +0800

    Trying to use ngx-bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AlertModule } from "ngx-bootstrap";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { DepositeProductDetailsComponent } from "./deposite-product-details/deposite-product-details.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AlertModule.forRoot()
+  ],
   declarations: [AppComponent, HelloComponent, DepositeProductDetailsComponent],
   bootstrap: [AppComponent]
 })
