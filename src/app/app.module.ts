37095c39c8dade60c02ad4ab97e656286db7bbd0
jarvisji <jarvis.ji@outlook.com>
2020-03-15 16:23:30 +0800

    Create DepositeProductDetails component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DepositeProductDetailsComponent } from './deposite-product-details/deposite-product-details.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, DepositeProductDetailsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/deposite-product-details/deposite-product-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deposite-product-details',
+  templateUrl: './deposite-product-details.component.html',
+  styleUrls: ['./deposite-product-details.component.css']
+})
+export class DepositeProductDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
