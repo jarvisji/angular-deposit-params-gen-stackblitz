c69039f1a816451af7733c7a9cd1589220029ad3
jarvisji <jarvis.ji@outlook.com>
2020-03-16 22:33:10 +0800

    Minor updates for import ngx-bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AlertModule } from "ngx-bootstrap";
+import { AlertModule } from "ngx-bootstrap/alert";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
